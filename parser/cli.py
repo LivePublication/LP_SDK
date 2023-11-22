7c36da424685d206ba3dbd60b199f6c354fb428f
Nelis Drost <nelis.drost@gmail.com>
2023-11-22 16:30:51 +1300

    List subcrates command

--- parser/cli.py
+++ parser/cli.py
@@ -1,8 +1,19 @@
+import os
+
 import click
+import pathlib
 from rocrate.rocrate import ROCrate
 
 
-@click.command()
+@click.group()
 def cli():
-    click.echo('Hello World2!')
+    pass
 
+
+@cli.command()
+@click.argument('path', type=click.Path(exists=True, file_okay=False, path_type=pathlib.Path))
+def list_subcrates(path):
+    # Find all ro-crate-metadata.json (by name)
+    for root, dir, files in os.walk(path):
+        if 'ro-crate-metadata.json' in files:
+            print(pathlib.Path(root) / 'ro-crate-metadata.json')
