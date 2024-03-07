afc5d22c68972e1f5417c781a998b8c3455eaadf
Nelis Drost <seregon888@gmail.com>
2024-03-07 14:53:16 +1300

    Basic prospective rocrate generation - CLI integration

--- parser/cli.py
+++ parser/cli.py
@@ -1,14 +1,35 @@
 import os
+from pathlib import Path
 
 import click
 import pathlib
 
 from parser.crate import get_crates
-
+from parser import prospective as _prospective
 
 @click.group()
 def cli():
     pass
+
+
+@cli.command()
+@click.option('-i', '--input',
+              type=click.Path(exists=True, file_okay=True, dir_okay=False, path_type=Path),
+              help='Input WEP')
+@click.option('-o', '--output',
+              type=click.Path(exists=False, file_okay=True, dir_okay=False, path_type=Path),
+              help='Output ROCrate')
+def prospective(input, output):
+    """
+    Very basic example of crate generation from WEP - nothing here is correct, everything will change
+    :param input:
+    :param output:
+    :return:
+    """
+    data = _prospective.load_wep(input)
+    result = _prospective.parse_wep_to_rocrate(data)
+    assert _prospective.validate_rocrate(result)
+    _prospective.write_rocrate(result, output)
 
 
 @cli.command()
