22efcf5b4a03ace9048643ed2b98f8ef0d4a6685
Nelis Drost <seregon888@gmail.com>
2024-03-21 14:37:32 +1300

    ConformsTo attributes for metadata and root dataset

--- provenance/crate.py
+++ provenance/crate.py
@@ -16,6 +16,22 @@ class LpProvCrate:
         self.path = Path(path)
         self.crate = ROCrate()
 
+        # Conforms to provenance crate
+        profiles = [
+            {'@id': 'https://w3id.org/ro/crate/1.1'},
+            {'@id': 'https://w3id.org/workflowhub/workflow-ro-crate/1.0'}
+        ]
+        self.crate.metadata['conformsTo'] = profiles
+
+        # Root dataset conforms to provenance crate
+        profiles = [
+            {"@id": "https://w3id.org/ro/wfrun/process/0.1"},
+            {"@id": "https://w3id.org/ro/wfrun/workflow/0.1"},
+            {"@id": "https://w3id.org/ro/wfrun/provenance/0.1"},
+            {"@id": "https://w3id.org/workflowhub/workflow-ro-crate/1.0"}
+        ]
+        self.crate.root_dataset['conformsTo'] = profiles
+
     def add_file(self, path: str):
         self.crate.add_file(path)
 
