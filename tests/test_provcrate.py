061c1d35b90ac1ef3a5fa25e90ed53126bfd3e0f
Nelis Drost <nelis.drost@gmail.com>
2024-04-19 15:20:29 +1200

    Add convoluted checking - to exclude references to retrospective entities from validation

--- tests/test_provcrate.py
+++ tests/test_provcrate.py
@@ -13,35 +13,70 @@ def _throw_or_print(msg, error=True, indent=0):
         print(' ' * indent + msg)
 
 
-def _compare_dicts(d1, d2, name='root', error=True, indent=0):
-    for key in d1:
-        if '@type' in d1[key] and detect_crate_type(d1[key]) == CrateParts.retrospective:
+def _exclude_item(item, graph: dict = None) -> bool:
+    # Item is in the graph, and is a retrospective type
+    if '@type' in item and detect_crate_type(item) == CrateParts.retrospective:
+        return True
+    # Item is a link to the graph, and item in graph is retrospective
+    if graph is not None:
+        if '@id' in item and _exclude_item(graph.get(item['@id'], {})):
+            return True
+    return False
+
+
+def _filter_list(item, graph: dict = None) -> dict:
+    return {i['@id']: i for i in item if not _exclude_item(i, graph)}
+
+
+def _is_graph_like(item) -> bool:
+    return isinstance(item, list) and len(item) > 0 and isinstance(item[0], dict) and '@id' in item[0]
+
+
+def _compare_dicts(expected, actual, name='root', error=True, indent=0, graph=None):
+    if graph is None:
+        graph = {item['@id']: item for item in expected['@graph']}
+
+    for key in expected:
+        if _exclude_item(expected[key], graph):
             print(' ' * indent + key + '- skipped, not prospective')
             continue
         else:
             print(' ' * indent + key)
 
-        if key not in d2:
-            _throw_or_print(f"Key {key} not in {name}: {d2}", error, indent+2)
+        if key not in actual:
+            if _is_graph_like(expected[key]) and len(_filter_list(expected[key], graph)) == 0:
+                # All items in list are retrospective
+                continue
+            _throw_or_print(f"Key {key} not in {name}: {actual}", error, indent + 2)
             continue
 
-        if isinstance(d1[key], dict):
-            _compare_dicts(d1[key], d2[key], key, error, indent+2)
-        elif isinstance(d1[key], list):
-            if key == '@graph':
+        if isinstance(expected[key], dict):
+            _compare_dicts(expected[key], actual[key], key, error, indent + 2, graph=graph)
+        elif isinstance(expected[key], list):
+            if isinstance(expected[key][0], dict):#key == '@graph':
                 # Build dicts of items by id
-                if not isinstance(d2[key], list):
-                    _throw_or_print(f"Item {key}: {d2[key]} is not a list", error, indent+2)
+                if not isinstance(actual[key], list):
+                    _throw_or_print(f"Item {key}: {actual[key]} is not a list", error, indent + 2)
                     continue
-                d1_items = {item['@id']: item for item in d1[key]}
-                d2_items = {item['@id']: item for item in d2[key]}
-                _compare_dicts(d1_items, d2_items, key, error, indent+2)
+
+                # if key == '@graph':
+                #     d1 = {item['@id']: item for item in expected[key]}
+                d1_items = {}
+                for item in expected[key]:
+                    if _exclude_item(item, graph):
+                        # Don't check for items in lists that would not have been added at prospective time
+                        # TODO - build a proper validator
+                        continue
+                    else:
+                        d1_items[item['@id']] = item
+                d2_items = {item['@id']: item for item in actual[key]}
+                _compare_dicts(d1_items, d2_items, key, error, indent+2, graph=graph)
             else:
-                if d1[key] != d2[key]:
-                    _throw_or_print(f"{d1[key]} != {d2[key]}", error, indent+2)
+                if expected[key] != actual[key]:
+                    _throw_or_print(f"{expected[key]} != {actual[key]}", error, indent + 2)
         else:
-            if d1[key] != d2[key]:
-                _throw_or_print(f"{d1[key]} != {d2[key]}", error, indent)
+            if expected[key] != actual[key]:
+                _throw_or_print(f"{expected[key]} != {actual[key]}", error, indent)
 
 
 def test_create_prov_crate():
