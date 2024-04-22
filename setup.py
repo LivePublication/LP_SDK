95690101bbc10dfb51bec6c4dd59a0b74b70892f
Nelis Drost <nelis.drost@gmail.com>
2024-04-23 10:28:56 +1200

    Update package name to lp-sdk

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="lp-parser",
+    name="lp-sdk",
     version="0.0.1",
     py_modules=find_packages(),
     install_requires=[
@@ -10,7 +10,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            "lp-parser = parser.cli:cli"
+            "lp-sdk = parser.cli:cli"
         ]
     }
 )
