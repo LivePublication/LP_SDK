3ffedb11fbdb7e13229a23b16a0d78a7b916e467
Nelis Drost <nelis.drost@gmail.com>
2024-04-22 14:47:40 +1200

    Print path when comparator fails

--- validation/validator.py
+++ validation/validator.py
@@ -24,9 +24,10 @@ class Comparator:
         """Filter out items that are not in the expected graph, or are of a type we do not care about"""
         return [item for item in items if self._consider_id(item)]
 
-    def _compare_dicts(self, expected: dict, actual: dict) -> bool:
+    def _compare_dicts(self, expected: dict, actual: dict, path: list) -> bool:
         """Compare two dictionaries, returns True if they match for the expected parts"""
         for key in expected:
+            _path = [*path, key]
             if not self._consider_id(expected[key]):
                 continue
 
@@ -36,10 +37,10 @@ class Comparator:
                     filtered = [item for item in expected[key] if self._consider_id(item)]
                     if len(filtered) == 0:
                         continue
-                raise AssertionError(f"Key {key} not in actual: {actual}")
+                raise AssertionError(f'Path: {" | ".join(_path)}\nKey {key} not in actual: {actual}')
 
             if isinstance(expected[key], dict):
-                self._compare_dicts(expected[key], actual[key])
+                self._compare_dicts(expected[key], actual[key], _path)
             elif isinstance(expected[key], list):
                 if isinstance(expected[key][0], dict):
                     # List of dicts, compare each item by key
@@ -49,29 +50,32 @@ class Comparator:
                         a_dict = {item['@id']: item for item in actual[key]}
                     else:
                         a_dict = {actual[key]['@id']: actual[key]}
-                    self._compare_dicts(e_dict, a_dict)
+                    self._compare_dicts(e_dict, a_dict, _path)
                 else:
                     # Not a list of dicts - compare directly
-                    assert expected[key] == actual[key], f"Value {key}: {expected[key]} does not match: {actual[key]}"
+                    assert expected[key] == actual[key], \
+                        f'Path: {" | ".join(_path)}\nValue {key}: {expected[key]} does not match: {actual[key]}'
             else:
                 # Not a dict or a list - compare values directly
-                assert expected[key] == actual[key], f"Value {key}: {expected[key]} does not match: {actual[key]}"
+                assert expected[key] == actual[key], \
+                    f'Path: {" | ".join(_path)}\nValue {key}: {expected[key]} does not match: {actual[key]}'
 
         return True
 
     def compare(self, actual: dict) -> bool:
         """Compare two partial crates, returns True if they match for the expected parts"""
-        assert actual['@context'] == self.expected['@context'], "Contexts do not match\n{actual['@context']}\n{self.expected['@context']}"
+        assert actual['@context'] == self.expected['@context'], \
+            "Contexts do not match\n{actual['@context']}\n{self.expected['@context']}"
 
         actual_graph = {item['@id']: item for item in actual['@graph']}
         for key in self.graph_dict:
+            path = ['@graph', key]
             if not self._consider_id(self.graph_dict[key]):
                 continue
 
             if key not in actual_graph:
-                raise AssertionError(f"Key {key} not in actual: {actual}")
+                raise AssertionError(f'Path: {" | ".join(path)}\nKey {key} not in actual: {actual}')
 
-            self._compare_dicts(self.graph_dict[key], actual_graph[key])
+            self._compare_dicts(self.graph_dict[key], actual_graph[key], path)
 
         return True
-
