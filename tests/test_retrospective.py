cf9ae40c684c73b9784ef66915445a3c9400758b
Nelis Drost <nelis.drost@gmail.com>
2024-04-26 11:52:41 +1200

    Retrospective: failing test

--- tests/test_retrospective.py
+++ tests/test_retrospective.py
@@ -1,7 +1,30 @@
+import json
 import tempfile
 from pathlib import Path
 
 from lp_sdk.parser.retrospective import format_retro_rocrate, write_retro_rocrate
+from lp_sdk.validation.util import CrateParts
+from lp_sdk.validation.validator import Comparator
+
+
+def test_create_retro_crate():
+    """TDD: manual creation of the retrospective parts of the crate"""
+    with tempfile.TemporaryDirectory() as d:
+        out_file = Path(d) / 'ro_crate_metadata.json'
+
+        crate = format_retro_rocrate({})
+        write_retro_rocrate(crate, out_file)
+        assert out_file.exists()
+
+        with open(Path(d) / 'ro-crate-metadata.json') as f:
+            actual = json.load(f)
+
+    with open(Path(__file__).parent / 'data' / 'cwl_prov' / 'ro-crate-metadata.json') as f:
+        expected = json.load(f)
+
+    comp = Comparator([CrateParts.retrospective],
+                      expected)
+    comp.compare(actual)
 
 
 def test_retrospective():
